cd4960f9da163a3c6cc3f4486906c1633d5cc191
Fredrick Peter <tamdevelopers@gmail.com>
2023-02-16 20:54:12 -0800

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,7 @@
-#include <stdio.h>
+#include <unistd.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-
-	return (1);
+int main(void) {
+  const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+  write(2, message, 48);
+  return 1;
 }
